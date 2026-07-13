c9ee7cddfdf669800feba823d363e958faef3577
takacsflora <40361594+takacsflora@users.noreply.github.com>
2026-07-13 15:04:32 +0100

    typo

--- exploratory_visaulization/ephys/passive_pop_scores.py
+++
@@ -1,7 +0,0 @@
-#%%
-from NeuroLogit.src.ephys.encoding_avg.results_helpers import read_in_all_coefs
-import pandas as pd
-
-coefs,models = read_in_all_coefs(fit_type='passive', subset='', recompute=False, add_behav_params=True, get_best=True)
-
-# %%
